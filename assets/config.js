48de3944672a19d21a923584fe24447b95030294
lukaschare <854450978@qq.com>
2026-09-25 16:40:07 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -5,7 +5,7 @@ window.LTS_CONFIG = {
   email: "lukaschare@gmail.com",
   phone: "+34 645 514 715",
   city: "Barcelona, Spain",
-  businessAddress: "Calle de la Vinyeta 55, Ático 1, 08904 L’Hospitalet de Llobregat, Barcelona, España",
+  businessAddress: "Calle de la Vinyeta 15, Ático 1, 08904 L’Hospitalet de Llobregat, Barcelona, España",
   domain: "lukastechsolutions.es",
   linkedin: "https://www.linkedin.com/in/junjun-lu-40b7a9113/",
   lastUpdated: "September 2026"
